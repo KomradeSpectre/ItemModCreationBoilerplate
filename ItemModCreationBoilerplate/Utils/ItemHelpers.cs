04d94dc93b10f53c508a50f0acb7c83305801c53
jchurch093@gmail.com <jchurch093@gmail.com>
2021-07-03 12:23:52 -0400

    Update R2API version, upgrade itemdisplaysetup, introduce material controller components with option to attach while using ItemDisplaySetup.

--- ItemModCreationBoilerplate/Utils/ItemHelpers.cs
+++ ItemModCreationBoilerplate/Utils/ItemHelpers.cs
@@ -1,4 +1,6 @@
-﻿using RoR2;
+﻿using ItemModCreationBoilerplate.Utils.Components;
+using RoR2;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ItemModCreationBoilerplate.Utils
@@ -10,18 +12,33 @@ namespace ItemModCreationBoilerplate.Utils
         /// <para>This allows it to go invisible when your character is not visible, as well as letting overlays affect it.</para>
         /// </summary>
         /// <param name="obj">The GameObject/Prefab that you wish to set up RendererInfos for.</param>
+        /// <param name="debugmode">Do we attempt to attach a material shader controller instance to meshes in this?</param>
         /// <returns>Returns an array full of RendererInfos for GameObject.</returns>
-        public static CharacterModel.RendererInfo[] ItemDisplaySetup(GameObject obj)
+        public static CharacterModel.RendererInfo[] ItemDisplaySetup(GameObject obj, bool debugmode = false)
         {
-            MeshRenderer[] meshes = obj.GetComponentsInChildren<MeshRenderer>();
-            CharacterModel.RendererInfo[] renderInfos = new CharacterModel.RendererInfo[meshes.Length];
 
-            for (int i = 0; i < meshes.Length; i++)
+            List<Renderer> AllRenderers = new List<Renderer>();
+
+            var meshRenderers = obj.GetComponentsInChildren<MeshRenderer>();
+            if (meshRenderers.Length > 0) { AllRenderers.AddRange(meshRenderers); }
+
+            var skinnedMeshRenderers = obj.GetComponentsInChildren<SkinnedMeshRenderer>();
+            if (skinnedMeshRenderers.Length > 0) { AllRenderers.AddRange(skinnedMeshRenderers); }
+
+            CharacterModel.RendererInfo[] renderInfos = new CharacterModel.RendererInfo[AllRenderers.Count];
+
+            for (int i = 0; i < AllRenderers.Count; i++)
             {
+                if (debugmode)
+                {
+                    var controller = AllRenderers[i].gameObject.AddComponent<MaterialControllerComponents.HGControllerFinder>();
+                    controller.Renderer = AllRenderers[i];
+                }
+
                 renderInfos[i] = new CharacterModel.RendererInfo
                 {
-                    defaultMaterial = meshes[i].material,
-                    renderer = meshes[i],
+                    defaultMaterial = AllRenderers[i] is SkinnedMeshRenderer ? AllRenderers[i].sharedMaterial : AllRenderers[i].material,
+                    renderer = AllRenderers[i],
                     defaultShadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On,
                     ignoreOverlays = false //We allow the mesh to be affected by overlays like OnFire or PredatoryInstinctsCritOverlay.
                 };
