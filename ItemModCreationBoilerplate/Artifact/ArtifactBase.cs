dac47e5fe9d1cfc8cbf7114ef39ea01aefe420b2
jchurch093@gmail.com <jchurch093@gmail.com>
2021-08-08 11:13:38 -0400

    Update R2API, add comment to ArtifactBase.cs

--- ItemModCreationBoilerplate/Artifact/ArtifactBase.cs
+++ ItemModCreationBoilerplate/Artifact/ArtifactBase.cs
@@ -22,6 +22,7 @@ namespace ItemModCreationBoilerplate.Artifact
 
         public ArtifactDef ArtifactDef;
 
+        //For use only after the run has started.
         public bool ArtifactEnabled => RunArtifactManager.instance.IsArtifactEnabled(ArtifactDef);
 
         public abstract void Init(ConfigFile config);
